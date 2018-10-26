c1cedd0b675005b4de462b25580b14fcb44ac11b
Denis Antonov <antonov@appvelox.ru>
2018-10-26 11:03:18 +0300

    end models testing

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 
 from vibermsg.models.keyboards import Button, Keyboard
 from vibermsg.models.messages import TextMessage
-from vibermsg.models.requests import (TextMessageRequest, IncomingTextMessage, Sender, ConversationStartedRequest, Location)
+from vibermsg.models.requests import (Sender, Location, ConversationStartedRequest, TextMessageRequest, IncomingTextMessage)
 
 
 class TestKeybooards:
@@ -131,4 +131,91 @@ class TestRequests:
             Location(1.2, '')
 
     def test_Sender(self):
-        pass
+        Sender(**{
+            "id": "01234567890A=",
+            "name": "John McClane",
+            "avatar": "http://avatar.example.com",
+            "country": "UK",
+            "language": "en",
+            "api_version": 1
+        })
+        with pytest.raises(TypeError):
+            Sender(**{
+                "id": 1,
+                "name": "John McClane",
+                "avatar": "http://avatar.example.com",
+                "country": "UK",
+                "language": "en",
+                "api_version": 1
+            })
+        with pytest.raises(TypeError):
+            Sender(**{
+                "id": "01234567890A=",
+                "name": 1,
+                "avatar": "http://avatar.example.com",
+                "country": "UK",
+                "language": "en",
+                "api_version": 1
+            })
+        with pytest.raises(TypeError):
+            Sender(**{
+                "id": "01234567890A=",
+                "name": "John McClane",
+                "avatar": 1,
+                "country": "UK",
+                "language": "en",
+                "api_version": 1
+            })
+        with pytest.raises(TypeError):
+            Sender(**{
+                "id": "01234567890A=",
+                "name": "John McClane",
+                "avatar": "http://avatar.example.com",
+                "country": 1,
+                "language": "en",
+                "api_version": 1
+            })
+        with pytest.raises(TypeError):
+            Sender(**{
+                "id": "01234567890A=",
+                "name": "John McClane",
+                "avatar": "http://avatar.example.com",
+                "country": "UK",
+                "language": 1,
+                "api_version": 1
+            })
+
+    def test_ConversationStartedRequest(self):
+        sender = Sender(**{
+            "id": "01234567890A=",
+            "name": "John McClane",
+            "avatar": "http://avatar.example.com",
+            "country": "UK",
+            "language": "en",
+            "api_version": 1
+        })
+
+        ConversationStartedRequest(sender, 1)
+        with pytest.raises(TypeError):
+            ConversationStartedRequest(1, 1)
+        with pytest.raises(TypeError):
+            ConversationStartedRequest(sender, '')
+
+    def test_IncomingTextMessage(self):
+        IncomingTextMessage('test')
+        with pytest.raises(TypeError):
+            IncomingTextMessage(1)
+
+    def test_TextMessageRequest(self):
+        message = IncomingTextMessage('')
+        sender = Sender(**{
+            "id": "01234567890A=",
+            "name": "John McClane",
+            "avatar": "http://avatar.example.com",
+            "country": "UK",
+            "language": "en",
+            "api_version": 1
+        })
+        TextMessageRequest(sender, message, 1)
+        with pytest.raises(TypeError):
+            TextMessageRequest(sender, 1, 1)
